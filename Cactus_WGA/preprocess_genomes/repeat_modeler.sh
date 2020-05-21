55960bb398d53cb68a78496f6d85317c01acff9f
Rory Craig <rory.craig@ed.ac.uk>
2020-05-21 18:18:31 +0100

    Update repeat_modeler.sh

--- Cactus_WGA/preprocess_genomes/repeat_modeler.sh
+++ Cactus_WGA/preprocess_genomes/repeat_modeler.sh
@@ -2,4 +2,5 @@ for i in *fa ; do
 	root=`basename $i .fa`
   BuildDatabase -name $root -engine ncbi $i
   RepeatModeler -pa 20 -database $root
+  perl ../../repeat_masking/c_incerta/renameRMDLconsensi.pl $root-families.fa $root $root_repmod.fa
 done
